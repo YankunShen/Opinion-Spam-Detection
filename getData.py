0f8fabfe5a4ef54a95a43f7545a266fc6b72c978
YankunShen <yankun@uw.edu>
2020-02-24 11:44:59 -0800

    get data

---
+++ getData.py
@@ -0,0 +1,21 @@
+import os
+
+# combine all separate files into one whole file
+
+# get all file names
+fnames = []
+
+for root, d_names, f_names in os.walk('/Users/shenyankun/Downloads/op_spam_v1.4/positive_polarity/truthful_from_TripAdvisor'):
+    for f in f_names:
+        fnames.append(os.path.join(root, f))
+
+print('fnames=', fnames)
+
+with open('/Users/shenyankun/Downloads/op_spam_v1.4/positive_polarity/truthful_from_TripAdvisor.txt', 'w') as f:
+    for file_name in fnames:
+        if ".DS_Store" in file_name:
+            continue
+        file = open(file_name, 'r')
+        data = file.read()
+        f.write(data)
+        f.write('\n')
